f6736e7e12672f16a1a2f038c8e1dcb8b977bbd2
ViihpropreyKnG <vinicius.ribeirosiqu@horowhenua.school.nz>
2025-03-17 14:32:25 +1300

    Delete B-01.py

--- B-01.py
+++
@@ -1,8 +0,0 @@
-# Functions go here
-def make_statement(statement, decoration):
-   """Emphasises headings by adding decoration at the start and end"""
-
-   print(f"{decoration * 3} {statement} {decoration *3}")
-
-make_statement(statement= "Viihproprey!", decoration= "👑")
-
